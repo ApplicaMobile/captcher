1bc3d0c91e3f3bbb1d6e7e94e14e6076ed580f9a
Rodrigo Castro <rcastro@applica.dev>
2025-05-09 14:10:01 -0300

    Cambio de puerto y .env example

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { CaptchaModule } from './captcha.module';
 async function bootstrap() {
   const app = await NestFactory.create(CaptchaModule);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
